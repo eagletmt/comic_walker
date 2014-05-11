ce0b4bfdbb2af0767e1921080be16bcbf415583f
Kohei Suzuki <eagletmt@gmail.com>
2014-05-11 21:57:32 +0900

    Extract out image_dir

--- lib/comic_walker/content_downloader.rb
+++ lib/comic_walker/content_downloader.rb
@@ -7,14 +7,8 @@ module ComicWalker
 
     def save
       license = @client.get_license(@cid)
-      info = license.get_info.first
-      last_update = Time.strptime(info['last_update'], '%Y%m%d%H%M')
-      title = info['issues'].first['content_name']
-
       decoder = ItemDecoder.new(license.get_configuration_pack)
-      pages = []
-
-      img_dir = Pathname.new(title).join(@cid).tap(&:mkpath)
+      img_dir = image_dir(license)
       decoder.pages.each.with_index do |file, i|
         dat_path = Pathname.new(file).join('0.dat')
         img_fname = dat_path.parent.basename.sub_ext('.jpg')
@@ -22,6 +16,12 @@ module ComicWalker
         decoder.decode(file, dat_path, img_path, license.get_jpeg(file))
         puts "#{dat_path} -> #{img_path}"
       end
+    end
+
+    def image_dir(license)
+      info = license.get_info.first
+      title = info['issues'].first['content_name']
+      Pathname.new(title).join(@cid).tap(&:mkpath)
     end
   end
 end
